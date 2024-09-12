dd51f2d537a048468c02c32fd4c0b767b21fbf96
RamJi <159413326+Ramvaidtheking@users.noreply.github.com>
2024-09-12 17:00:15 +0530

    Add files via upload

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Gallery = () => {
+  const gallery = [
+    "/c:\Users\ramsa\Downloads\WhatsApp Image 2024-09-11 at 22.48.04_27b85951.jpg",
+    "/img2.jpg",
+    "/img3.jpg",
+    "/img4.jpg",
+    "/img7.jpg",
+    "/img8.jpg",
+    "/img5.jpg",
+    "/img6.jpg",
+  ];
+  return (
+    <section className="gallery">
+      <h1>BETTER BEATS BEST</h1>
+      <div className="images">
+        <div>
+          {gallery.slice(0, 3).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+        <div>
+          {gallery.slice(3, 6).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+        <div>
+          {gallery.slice(6, 9).map((element, index) => (
+            <img key={index} src={element} alt="galleryImage" />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>Ram Ji Fitness</p>
+    </header>
+  );
+};
+
+export default Navbar;
